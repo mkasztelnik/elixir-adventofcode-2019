21be1dc14747bfe8a714a353219b4218dea3353e
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-12-06 10:49:30 +0100

    d06 p2

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -39,6 +39,45 @@ defmodule AdventOfCode.Day06 do
     object_orbits_count(map, tail ++ Map.get(map, object, []), sum + 1)
   end
 
+
+  @doc """
+    Day 06 part 1
+
+   ## Examples:
+    iex> AdventOfCode.Day06.part2(
+    iex>   ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G",
+    iex>    "G)H", "D)I", "E)J", "J)K", "K)L", "K)YOU", "I)SAN"])
+    4
+  """
   def part2(args) do
+    args
+    |> build_reversed_map()
+    |> jumps_count()
   end
+
+  defp build_reversed_map(map_entries) do
+    map_entries
+    |> Enum.reduce(%{}, fn map_entry, map ->
+      [object1, object2] = String.split(map_entry, ")")
+      Map.put(map, object2, object1)
+    end)
+  end
+
+  defp jumps_count(reversed_map) do
+    santa_jumps = jumps_for(reversed_map, "SAN")
+    my_jumps = jumps_for(reversed_map, "YOU")
+    {santa_transfers, my_transfers} = diff(santa_jumps, my_jumps)
+
+    length(santa_transfers) + length(my_transfers)
+  end
+
+  defp jumps_for(reversed_map, object, path \\ []) do
+    case Map.get(reversed_map, object) do
+      nil  -> path
+      jump -> jumps_for(reversed_map, jump, [jump | path])
+    end
+  end
+
+  def diff([object | tail1], [object | tail2]), do: diff(tail1, tail2)
+  def diff(list1, list2), do: {list1, list2}
 end
