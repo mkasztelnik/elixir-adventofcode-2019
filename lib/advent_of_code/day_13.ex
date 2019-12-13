2a06ef5032fc44d6a91b5e8511a77ab665909cd6
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-12-14 00:26:39 +0100

    d13 p1

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -1,5 +1,17 @@
 defmodule AdventOfCode.Day13 do
-  def part1(args) do
+  def part1(code) do
+    my_pid = self()
+    pid = spawn(fn -> AdventOfCode.Intcode.run(code, my_pid) end)
+
+    read()
+    |> Enum.count(fn [type | _] -> type == 2 end)
+  end
+
+  defp read(outputs \\ []) do
+    receive do
+      {:output, output} -> read([output | outputs])
+      :eot -> Enum.chunk_every(outputs, 3)
+    end
   end
 
   def part2(args) do
