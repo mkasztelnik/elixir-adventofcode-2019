7e70f1b0e704a0a92bde63f5b13edb3dd7c916a2
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-12-16 23:40:14 +0100

    d15 p2

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -2,10 +2,16 @@ defmodule AdventOfCode.Day15 do
   @moves [1, 2, 3, 4]
 
   def part1(code) do
-    current_possition = {0, 0}
-    current_board = %{current_possition => {1, 0, AdventOfCode.Intcode.parse(code)}}
+    {_, cost, _} = find_oxygen(code)
 
-    find(current_board, [current_possition], MapSet.new())
+    cost
+  end
+
+  defp find_oxygen(code) do
+    current_possition = {0, 0}
+
+    %{current_possition => {1, 0, AdventOfCode.Intcode.parse(code)}}
+    |> find([current_possition], MapSet.new())
   end
 
   defp find(board, [possition | targets], visited) do
@@ -25,7 +31,7 @@ defmodule AdventOfCode.Day15 do
         )
 
       2 ->
-        cost
+        {possition, cost, state}
     end
   end
 
@@ -56,6 +62,34 @@ defmodule AdventOfCode.Day15 do
   defp next_possition(3, {x, y}), do: {x - 1, y}
   defp next_possition(4, {x, y}), do: {x + 1, y}
 
-  def part2(args) do
+  def part2(code) do
+    {oxygen_possition, _, state} = find_oxygen(code)
+
+    {_, {_, cost, _}} =
+      %{oxygen_possition => {2, 0, state}}
+      |> build_map([oxygen_possition], MapSet.new())
+      |> Enum.max_by(fn {_, {type, cost, _}} -> if type == 1, do: cost, else: 0 end)
+
+    cost
+  end
+
+  defp build_map(board, [], _visited), do: board
+
+  defp build_map(board, [possition | targets], visited) do
+    {type, _cost, _state} = Map.get(board, possition)
+
+    case type do
+      0 ->
+        build_map(board, targets, MapSet.put(visited, possition))
+
+      _ ->
+        updated_board = board |> put_moves(possition)
+
+        build_map(
+          updated_board,
+          targets ++ next_possible_moves(updated_board, possition, visited),
+          MapSet.put(visited, possition)
+        )
+    end
   end
 end
