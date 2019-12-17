93e4abdbae8d89da1b7098c04cd53b30a9fd4b29
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-12-17 01:11:43 +0100

    d16 p2

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -1,7 +1,96 @@
 defmodule AdventOfCode.Day16 do
-  def part1(args) do
+  @doc """
+    Day 16 part 1
+
+    ## Examples:
+      iex> AdventOfCode.Day16.part1("80871224585914546619083218645595")
+      "24176176"
+      iex> AdventOfCode.Day16.part1("19617804207202209144916044189917")
+      "73745418"
+      iex> AdventOfCode.Day16.part1("69317163492948606335995924319873")
+      "52432133"
+  """
+  def part1(line) do
+    list = line |> parse()
+
+    1..100
+    |> Enum.reduce(list, fn _, l -> fft(l) end)
+    |> Enum.take(8)
+    |> Enum.join("")
   end
 
-  def part2(args) do
+  defp parse(line) do
+    line |> String.codepoints() |> Enum.map(&String.to_integer/1)
+  end
+
+  defp fft(list) do
+    {_, result} =
+      list
+      |> Enum.reduce({0, []}, fn _, {index, values} ->
+        {index + 1, [item_fft(list, index) | values]}
+      end)
+
+    Enum.reverse(result)
+  end
+
+  defp item_fft(list, index) do
+    {_, multiplied} =
+      list
+      |> Enum.reduce({0, []}, fn v, {current, values} ->
+        {current + 1, [multiplier(current, index) * v | values]}
+      end)
+
+    multiplied
+    |> Enum.sum()
+    |> rem(10)
+    |> abs()
+  end
+
+  defp multiplier(current, index) do
+    div(current + 1, index + 1) |> rem(4) |> value()
+  end
+
+  defp value(0), do: 0
+  defp value(1), do: 1
+  defp value(2), do: 0
+  defp value(3), do: -1
+
+  @doc """
+    Day 16 part 2
+
+    ## Examples:
+      iex> AdventOfCode.Day16.part2("03036732577212944063491565474664")
+      "84462026"
+      iex> AdventOfCode.Day16.part2("02935109699940807407585447034323")
+      "78725270"
+      iex> AdventOfCode.Day16.part2("03081770884921959731165446850517")
+      "53553731"
+  """
+  def part2(line) do
+    offset = line |> String.slice(0, 7) |> String.to_integer()
+
+    list =
+      line
+      |> String.duplicate(10000)
+      |> String.slice(offset..-1)
+      |> parse()
+      |> Enum.reverse()
+
+    1..100
+    |> Enum.reduce(list, fn _, l -> fft2(l) end)
+    |> Enum.reverse()
+    |> Enum.take(8)
+    |> Enum.join("")
+  end
+
+  defp fft2(list) do
+    {_, updated_list} =
+      list
+      |> Enum.reduce({0, []}, fn v, {last, result} ->
+        current = last + v
+        {current, [current |> rem(10) | result]}
+      end)
+
+    Enum.reverse(updated_list)
   end
 end
--- lib/mix/tasks/d16.p2.ex
+++ lib/mix/tasks/d16.p2.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.D16.P2 do
 
   @shortdoc "Day 16 Part 2"
   def run(args) do
-    input = nil
+    input = AdventOfCode.input!("inputs/d16.txt") |> hd()
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
